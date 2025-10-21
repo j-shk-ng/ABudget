c00764adf3804b1d9607432b827442736f16c1dd
j-shk-ng <121907818+j-shk-ng@users.noreply.github.com>
2025-10-20 19:15:24 -0500

    Initial Commit

---
+++ ABudgetTests/ABudgetTests.swift
@@ -0,0 +1,17 @@
+//
+//  ABudgetTests.swift
+//  ABudgetTests
+//
+//  Created by Joshua King on 10/20/25.
+//
+
+import Testing
+@testable import ABudget
+
+struct ABudgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
